3e6312065cc689fca3294eb5e0d0564527689839
Joel Gecale <jgecale_5@yahoo.com>
2023-09-28 11:26:58 +0800

    added follow/unfollow function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -29,37 +29,65 @@ $.ajaxSetup({
 });
 
 
-$(document).on("click", ".js-toggle-modal", function(e){
+$(document).on("click", ".js-toggle-modal", function (e) {
     e.preventDefault();
     $(".js-modal").toggleClass("hidden")
 })
 
-$(document).on("click", ".js-submit", function(e){
+$(document).on("click", ".js-submit", function (e) {
     e.preventDefault();
-    text= $(".js-post-text").val().trim()
+    text = $(".js-post-text").val().trim()
 
-    if(!text){
+    if (!text) {
         return false;
-    }    
-
-
-$(".js-submit").prop("disabled", true).text("Posting!")
-$.ajax({
-    type: 'POST',
-    url: $(".js-post-text").data("post-url"),
-    data: {
-        text : text
-    },
-    success: (dataHtml) => {
-        $(".js-modal").addClass("hidden");
-        $("#posts-container").prepend(dataHtml);
-        $(".js-submit").prop("disabled", false).text("New Post");
-        $(".js-post-text").val("")
-    },
-
-    error: (error) => {
-        console.warn(error)
-        $(".js-submit").prop("disabled",false).text("Error");
     }
+
+
+    $(".js-submit").prop("disabled", true).text("Posting!")
+    $.ajax({
+        type: 'POST',
+        url: $(".js-post-text").data("post-url"),
+        data: {
+            text: text
+        },
+        success: (dataHtml) => {
+            $(".js-modal").addClass("hidden");
+            $("#posts-container").prepend(dataHtml);
+            $(".js-submit").prop("disabled", false).text("New Post");
+            $(".js-post-text").val("")
+        },
+
+        error: (error) => {
+            console.warn(error)
+            $(".js-submit").prop("disabled", false).text("Error");
+        }
+    })
 })
+
+.on("click",".js-follow", function (e) {
+    e.preventDefault();
+    const action=$(this).attr("data-action")
+
+    $.ajax({
+        type: 'POST',
+        url: $(this).data("url"),
+        data: {
+            action: action,
+            username: $(this).data("username")
+        },
+        success: (data) => {
+            console.log(data)
+            $(".js-follow").prop("disabled", false).text(data.wording);
+           if(action =="follow"){
+                $(this).attr("data-action","unfollow")
+          }
+        else{
+            $(this).attr("data-action","follow")
+          }
+        },
+
+        error: (error) => {
+            console.warn("This is the error ${error}")
+        }
+    })
 })
